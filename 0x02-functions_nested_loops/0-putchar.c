d81e9ddc6950338ecc3c29bc4701d5997cc55794
Andy <iamandycoal@gmail.com>
2023-03-23 02:43:46 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - prints the word _putchar
+ * _putchar : prints a single character to console
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
