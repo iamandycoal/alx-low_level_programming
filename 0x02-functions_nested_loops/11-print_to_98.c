a1d0e9799a6a8b8b87f9041a107529afd332f80e
Andy <iamandycoal@gmail.com>
2023-03-25 02:50:05 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * print_to_98 - function that prints all natural numbers
+ *
+ * @n: character to be checked
+ *
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	printf("\n");
+}
