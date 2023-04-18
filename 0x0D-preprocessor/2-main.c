557504b23a9ed0164c45c7bc409dd2d4d1231693
Andy <iamandycoal@gmail.com>
2023-04-17 18:15:36 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#include <stdio.h>
 
-#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+/**
+ *main - a function which prints the name of the file
+ *
+ *Return: always 0
+*/
 
-#endif
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
